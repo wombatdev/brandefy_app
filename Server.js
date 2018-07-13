f49eacad1c7829474bb5cf670097a0c4c6df53cb
James <james@team-wombat.com>
2018-07-12 23:51:10 -0400

    Small typo correction

--- Server.js
+++ Server.js
@@ -10,7 +10,7 @@ function REST(){
     self.connectMysql();
 };
 
-James add
+// James add
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3001");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
