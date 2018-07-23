0b4f34f8f65cb3e021dad38628f6ff3c1643f801
James <james@team-wombat.com>
2018-07-23 10:02:57 -0400

    Updated CORS address to dev env

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ function REST(){
 
 // James add
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://35.185.99.179");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
